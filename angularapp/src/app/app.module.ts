b9f5acbefc2a82a25c49343e24d25ae9784f0e3e
Lisa-467 <shelavanedipti@gmail.com>
2024-05-17 19:19:22 +0530

    componant creation
    componant creation

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Demo } from './demo/demo.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, Demo],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ angularapp/src/app/demo/demo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  template:
+    '<h1> Component Information </h1><br><h2> First Name</h2><br><h2> Last Name</h2>',
+})
+export class Demo {}
