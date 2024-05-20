654be17b64cd7e321aba7e87ce0121f6ae9c1eb7
Lisa-467 <shelavanedipti@gmail.com>
2024-05-20 14:45:42 +0530

    creating components
    creating components by cli

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -4,9 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Demo } from './demo/demo.component';
+import { Demo2 } from './demo2/demo2.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { AlertComponent } from './alert/alert.component';
 
 @NgModule({
-  declarations: [AppComponent, Demo],
+  declarations: [AppComponent, Demo, Demo2, HeaderComponent, BodyComponent, FooterComponent, AlertComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ angularapp/src/app/demo2/demo2.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo2',
+  template: `<h1>Demo Component</h1>
+    <button class="btn btn-primary">Save</button>`,
+})
+export class Demo2 {}
